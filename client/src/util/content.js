ec4e0f2f60a58f81108f3da1ac98d14a6b392874
Gboly <gbolahanahmed1@gmail.com>
2024-01-17 00:27:00 +0100

    feat: Create content for nav options

---
+++ client/src/util/content.js
@@ -0,0 +1,17 @@
+export const navOptions = [
+  {
+    desc: "About",
+    content: [],
+    columnType: "single-column",
+  },
+  {
+    desc: "FAQ",
+    content: [],
+    columnType: "single-column",
+  },
+  {
+    desc: "Contact",
+    content: [],
+    columnType: "single-column",
+  },
+];
