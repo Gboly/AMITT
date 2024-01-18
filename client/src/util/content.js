84629b79ef15b77f9eaef67271382e2cafda7854
Gboly <gbolahanahmed1@gmail.com>
2024-01-18 10:29:52 +0100

    fix: Update content and functions

--- client/src/util/content.js
+++ client/src/util/content.js
@@ -1,3 +1,7 @@
+import recruit from "../assets/recruit.png";
+import train from "../assets/train.png";
+import deploy from "../assets/deploy.png";
+
 export const navOptions = [
   {
     desc: "About",
@@ -13,5 +17,38 @@ export const navOptions = [
     desc: "Contact",
     content: [],
     columnType: "single-column",
+  },
+];
+
+export const actions = [
+  {
+    img: recruit,
+    header: "Recruit",
+    desc: "Individuals interested in joining our talent pool simply fill out our application form. Once submitted, our dedicated team meticulously reviews each application, considering skills, experience, and cultural fit. We understand the significance of effective communication, and that's why we reach out to promising applicants via email. This personalized approach ensures a transparent and efficient recruitment process, where each candidate is valued, considered, and guided through the next steps in their career journey.",
+  },
+  {
+    img: train,
+    header: "Train",
+    desc: "In our training programs, we focus specifically on middle-level operations. We go beyond basic skills, providing comprehensive professional development tailored to the needs of this crucial operational tier. Our dynamic learning environment is designed for individuals in mid-level roles to flourish, offering targeted training in technical expertise, soft skills, and industry-specific knowledge. We believe that continuous learning is essential for unlocking the full potential of professionals in middle operations.",
+  },
+  {
+    img: deploy,
+    header: "Deploy",
+    desc: "This stage is the culmination of our efforts, where talent meets opportunity. We strategically match trained individuals with roles that align with their strengths and ambitions. By ensuring a seamless transition from training to the professional arena, we enable our candidates to bring a distinctive edge to their new workplace.",
+  },
+];
+
+export const ourValues = [
+  {
+    header: "Stay Bold",
+    desc: "At AMITT, we embrace the spirit of staying bold in everything we do. This value is the driving force behind our innovative approach to challenges and opportunities alike. We encourage our team to think beyond conventional boundaries, to take risks, and to boldly pursue new ideas.",
+  },
+  {
+    header: "Consistently Learning",
+    desc: "Consistently learning is the heartbeat of our organization. We believe that knowledge is the foundation of growth, and we are dedicated to fostering a culture of continuous learning. Whether it's staying abreast of industry trends, acquiring new skills, or seeking innovative solutions, we encourage our team to be perpetual learners.",
+  },
+  {
+    header: "Upholding Excellence",
+    desc: "Upholding excellence is not just a goal; it's a standard that permeates every aspect of our company. We set high expectations for ourselves and hold each other accountable to deliver outstanding quality in our work. Excellence is not a one-time achievement but a continuous pursuit. From our recruitment processes to training methodologies and deployment strategies, we strive for nothing short of excellence. ",
   },
 ];
--- client/src/util/function.js
+++ client/src/util/function.js
@@ -1,1 +1,6 @@
 export const isDev = process.env.NODE_ENV === "development";
+
+export const chooseAtRandomFromArray = (array) => {
+    const position = Math.floor(Math.random() * array.length);
+    return array[position];
+  };
