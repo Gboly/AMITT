78e931599e090bb21a77b18925a1d7b0874b51ed
Gboly <gbolahanahmed1@gmail.com>
2024-01-20 09:28:26 +0100

    feat: Each page should be rendered from top on routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { useLayoutEffect } from "react";
 import "./App.css";
+import { useLocation } from "react-router-dom";
 
 function App({ children }) {
   // Treat Top level state here
+
+  const location = useLocation();
+
+  // Each page should be rendered from top.
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return <>{children}</>;
 }
 
