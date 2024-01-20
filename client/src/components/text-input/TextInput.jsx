c3f505197cb6c9afa73a6fcb587f93c1dac5c9a9
Gboly <gbolahanahmed1@gmail.com>
2024-01-20 13:53:45 +0100

    feat: Create TextInput component

---
+++ client/src/components/text-input/TextInput.jsx
@@ -0,0 +1,32 @@
+import "./text-input.css";
+
+const inputTypes = {
+  email: "email",
+};
+const textareaTypes = ["message"];
+const TextInput = ({ value, handleInput, name, label }) => {
+  return (
+    <div className="text-input-container">
+      <label htmlFor={name}>{label}</label>
+      {textareaTypes.includes(name) ? (
+        <textarea
+          id={name}
+          value={value}
+          onInput={handleInput}
+          rows={10}
+          // autoComplete="off"
+        />
+      ) : (
+        <input
+          type={inputTypes[name] || "text"}
+          id={name}
+          value={value}
+          onInput={handleInput}
+          autoComplete="off"
+        />
+      )}
+    </div>
+  );
+};
+
+export default TextInput;
