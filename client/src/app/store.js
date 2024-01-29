64a2b0cf96bf0851f8157412809c30901d292537
Gboly <gbolahanahmed1@gmail.com>
2024-01-29 17:44:11 +0100

    feat: Add application reducer to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api";
 import { isDev } from "../util/function";
+import { applicationReducer } from "./applicationSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    application: applicationReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
