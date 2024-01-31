fd7f3f64d448e69b9b74900f957fe832b92a98eb
Gboly <gbolahanahmed1@gmail.com>
2024-01-31 16:15:56 +0100

    feat: Place the success handling in Layout.jsx

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -2,10 +2,21 @@ import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import "./layout.css";
 import { Outlet } from "react-router-dom";
+import { getSuccessState } from "../app/applicationSlice";
+import Successful from "../components/successful/Successful";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Layout = () => {
+  const isSuccessful = useSelector(getSuccessState);
+
+  useEffect(() => {
+    isSuccessful && (document.body.style.overflow = "hidden");
+  }, [isSuccessful]);
+
   return (
     <>
+      {isSuccessful && <Successful />}
       <Navbar />
       {/* <ErrorBoundary FallbackComponent={Error}> */}
       <Outlet />
