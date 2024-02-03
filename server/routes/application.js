263ddd401036858055996c8fb4590113537ace94
Gboly <gbolahanahmed1@gmail.com>
2024-02-03 14:55:47 +0100

    feat: Configure multer & attach as route middleware

--- server/routes/application.js
+++ server/routes/application.js
@@ -2,11 +2,21 @@ import { Router } from "express";
 import {
   createApplication,
   getApplication,
+  // submitApplication,
 } from "../controllers/application.js";
+import multer from "multer";
 
 const router = Router();
 
-router.route("/").post(createApplication);
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const fileUploads = upload.fields([
+  { name: "resume", maxCount: 1 },
+  { name: "coverLetter", maxCount: 1 },
+]);
+
+router.post("/", fileUploads, createApplication);
+// router.post("/submit", submitApplication);
 router.get("/:id", getApplication);
 
 export default router;
