a4d499c9852797f8d70a6d63d81d49c9e586c3f3
Gboly <gbolahanahmed1@gmail.com>
2024-02-03 15:02:20 +0100

    fix: Update helper functions
    - extractDataFromBody
    The request body passed to the endpoint is a formdata and certain fields
    should not be passed to the database. This function handles that.
    
    - canSaveToDB
    The condition for submitting the application is different as it has to
    be confirmed that all other stages has been completed.
    
    - initialDriveFile
    This was created to cater to the schema for resume and coverLetter. In
    the absence of the optional coverLetter, a value that aligns with schema
    is passed.
    
    - uploadFile
    This is the function that uploads the file to google drive.
    
    Author - Ahmed Quamordeen Gbolahan <gbolahanahmed1@gmail.com>

--- server/util/helperFuntions.js
+++ server/util/helperFuntions.js
@@ -1,3 +1,8 @@
+import Application from "../models/application.js";
+import auth from "../config/googleConfig.js";
+import stream from "stream";
+import { google } from "googleapis";
+
 const requiredDataByStage = {
   1: [
     "firstName",
@@ -35,5 +40,51 @@ const requiredDataByStage = {
   7: [],
 };
 
-export const canSaveStageToDB = (stage, data) =>
-  requiredDataByStage[stage]?.every((key) => data[key]);
+const dataExemptions = ["id", "stage", "createdAt"];
+export const extractDataFromBody = (body) => {
+  return Object.entries(body).reduce((accum, [key, value]) => {
+    !dataExemptions.includes(key) && (accum = { ...accum, [key]: value });
+    return accum;
+  }, {});
+};
+
+const stages = [1, 2, 3, 4, 5, 6];
+export const canSaveStageToDB = async (stage, data, id) => {
+  if (stage !== 7) return requiredDataByStage[stage]?.every((key) => data[key]);
+
+  const application = await Application.findById(id);
+  if (!application) return false;
+
+  const completedStages = application.completedStagesdetails.map(
+    ({ stage }) => stage
+  );
+  return stages.every((stage) => completedStages.includes(stage));
+};
+
+export const uploadFile = async (fileObject) => {
+  if (!fileObject) return { name: "", id: "" };
+
+  const bufferStream = new stream.PassThrough();
+  bufferStream.end(fileObject.buffer);
+  const {
+    data: { name, id },
+  } = await google.drive({ version: "v3", auth }).files.create({
+    media: {
+      mimeType: fileObject.mimetype,
+      body: bufferStream,
+    },
+    requestBody: {
+      name: fileObject.originalname,
+      parents: ["1TmTPl5NoEfLUaGb4CmmqXTfbNYpPJh2Y"],
+    },
+    fields: "id,name",
+  });
+  // console.log(`Uploaded file ${name} ${id}`);
+  return { name, driveId: id, key: fileObject.fieldname };
+};
+
+const fileNames = ["resume", "coverLetter"];
+export const initialDriveFiles = fileNames.reduce((accum, name) => {
+  accum = { ...accum, [name]: { name: "", driveId: "" } };
+  return accum;
+}, {});
