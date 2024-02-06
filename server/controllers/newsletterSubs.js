cf77af6e0c6f9967dee545eef058ccbfe0c5226c
Gboly <gbolahanahmed1@gmail.com>
2024-02-06 16:16:39 +0100

    fix: Backend code to implement unsubscribe from mail

--- server/controllers/newsletterSubs.js
+++ server/controllers/newsletterSubs.js
@@ -7,14 +7,6 @@ dotenv.config();
 export const subscribe = async (req, res) => {
   const { name, email } = req.body;
 
-  const mailOptions = {
-    from: process.env.GMAIL,
-    to: email,
-    template: "newsletter",
-    subject: "You want to keep in touch",
-    context: { name },
-  };
-
   try {
     const canSave = Object.values({ ...req.body }).every((value) => value);
     if (!canSave) throw "Incomplete details";
@@ -27,10 +19,37 @@ export const subscribe = async (req, res) => {
     const subscriber = new NewsletterSubs({ ...req.body });
     await subscriber.save();
 
+    const mailOptions = {
+      from: process.env.GMAIL,
+      to: email,
+      template: "newsletter",
+      subject: "You want to keep in touch",
+      context: {
+        name,
+        unsubscribeLink: `http://localhost:5173/unsubscribe?id=${subscriber?._id}`,
+      },
+    };
     transport.use("compile", hbs(hbOptions));
     await transport.sendMail(mailOptions);
 
     res.status(200).json({ message: "Subscription successful." });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ error: "An error was encountered. Try again" });
+  }
+};
+
+export const unsubscribe = async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const isDeleted = await NewsletterSubs.findOneAndDelete({ _id: id });
+
+    if (!isDeleted) throw "Invalid subscriber Id";
+
+    res.status(200).json(req.body);
   } catch (error) {
     console.log(error);
     return res
--- server/routes/newsletterSubs.js
+++ server/routes/newsletterSubs.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { subscribe } from "../controllers/newsletterSubs.js";
+import { subscribe, unsubscribe } from "../controllers/newsletterSubs.js";
 
 const router = Router();
 
 router.post("/subscribe", subscribe);
+router.post("/unsubscribe", unsubscribe);
 
 export default router;
